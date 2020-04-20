a50ea85b6c9f4d824fe437c2a069f02dc569d8b8
Eduardo Almeida <me@edr.io>
2020-04-20 01:53:24 +0100

    Initial Commit

---
+++ FruityServer/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  FruityServer
+//
+//  Created by Eduardo Almeida on 20/04/2020.
+//  Copyright © 2020 Eduardo Almeida. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
